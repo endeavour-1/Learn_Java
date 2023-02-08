c2fbd04fac23c4a2f73ade97b4640f159ee96927
Jayesh Savkare <jayesh.savkare.work@gmail.com>
2023-02-08 21:14:49 +0530

    added latest files

---
+++ section two/Grades.java
@@ -0,0 +1,11 @@
+public class Grades {
+    public static void main(String[] args) {
+        char potions = 'A';
+        char arithmacy = 'B';
+        char charms = 'C';
+
+        System.out.println(" potions " + potions );
+        System.out.println(" arithmacy " + arithmacy );
+        System.out.println(" charms " + charms );
+    }
+}
