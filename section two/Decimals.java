3099c7e22dfec411f0aa709152f77de9680e084c
Jayesh Savkare <jayesh.savkare.work@gmail.com>
2023-02-12 15:59:28 +0530

    Learned Decimal Variable

---
+++ section two/Decimals.java
@@ -0,0 +1,10 @@
+public class Decimals {
+    public static void main(String[] args) {
+        double percentage = 73.5;
+        System.out.println(percentage +  " of percentages are made up");
+        double wallet = 20; //always use double for mathematical calculations. This will be stored as a double: 20.00
+        int people = 3;
+        System.out.println(wallet / people);
+    }
+}
+//Never Use integer for maths !!//
